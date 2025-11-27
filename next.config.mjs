6aa3a9c1a4f430888964fd0a1017a9209664b9a5
nazmul_khan <nazmul.khan121@gmail.com>
2025-11-27 10:56:08 +0600

    update next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/old-home',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/products-old/:id',
+        destination: '/products/:id',
+        permanent: true,
+      },
+      {
+        source: '/about-us-old',
+        destination: '/about',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
